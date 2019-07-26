6c846b8bb4a79583a6a30f3339be702bc95b1b19
honorine1 <uwurukundoh@gmail.com>
2019-07-26 17:45:58 +0200

    js indentation and spacing verified

--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,29 @@ $('.design').toggle();
 $('#desi').toggle();
 });
 });
-// $(document).ready(function(){
-    $('#desi').click(function(){
-    $('.design').toggle();
-    $('#desi').toggle();
-    });
-    // });
+$('#desi').click(function(){
+$('.design').toggle();
+$('#desi').toggle();
+});
     // for the second icon
-    // $(document).ready(function(){
-        $('#dev-icon').click(function(){
-        $('.development').toggle();
-        $('#develop').toggle();
-        });
-        // });
-        // $(document).ready(function(){
-            $('#develop').click(function(){
-            $('.development').toggle();
-            $('#develop').toggle();
-            });
-            // });
+$('#dev-icon').click(function(){
+$('.development').toggle();
+$('#develop').toggle();
+});
+$('#develop').click(function(){
+$('.development').toggle();
+$('#develop').toggle();
+});
             // for the third icon
-            // $(document).ready(function(){
-                $('#pro-icon').click(function(){
-                $('.product').toggle();
-                $('#prod').toggle();
-                });
-                // });
-                // // $(document).ready(function(){
-                    $('#prod').click(function(){
-                    $('.product').toggle();
-                    $('#prod').toggle();
-                    });
-                //    });
+ $('#pro-icon').click(function(){
+$('.product').toggle();
+$('#prod').toggle();
+});
+$('#prod').click(function(){
+$('.product').toggle();
+$('#prod').toggle();
+});
+
     // Portfolio scripts
 //first image
 $('#p1').hide();
