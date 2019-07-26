495b1903be72f9ac09c61dede0e594a42ce4d5b6
honorine1 <uwurukundoh@gmail.com>
2019-07-26 10:35:31 +0200

    toggling between image and paragraph of second icon is added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
+// for first icon
 $(document).ready(function(){
-$('.design').click(function(){
- $('.design').toggle();
+$('#des-icon').click(function(){
+$('.design').toggle();
 $('#desi').toggle();
 });
 });
 $(document).ready(function(){
     $('#desi').click(function(){
-    $('#desi').toggle();
     $('.design').toggle();
+    $('#desi').toggle();
     });
     });
+    // for the second icon
+    $(document).ready(function(){
+        $('#dev-icon').click(function(){
+        $('.development').toggle();
+        $('#develop').toggle();
+        });
+        });
+        $(document).ready(function(){
+            $('#develop').click(function(){
+            $('.development').toggle();
+            $('#develop').toggle();
+            });
+            });
+            // for the third icon
+            $(document).ready(function(){
+                $('#pro-icon').click(function(){
+                $('.product').toggle();
+                $('#prod').toggle();
+                });
+                });
+                $(document).ready(function(){
+                    $('#prod').click(function(){
+                    $('.product').toggle();
+                    $('#prod').toggle();
+                    });
+                    });
+    
